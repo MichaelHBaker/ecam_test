a2975ecb9a2c3d0ff1334c993b9f944081fe226e
MichaelHBaker <mbaker@sbwconsulting.com>
2023-10-17 09:01:38 -0700

    10-17-23 9-01

--- convertxml.py
+++ convertxml.py
@@ -32,10 +32,6 @@ with open("manifest_bs4.xml", "w") as f:
             
             for line in group.contents:
                 if type(line) == Tag:
-                    # button_tag
-                    # menu_open_tag
-                    # menu_close_tag
-                    # menu_button_tag
                     if line.name == 'button':
                         group_xml.append(f'''  <Control xsi:type="Button" id="{line['id']}">''')
                         group_xml.append(f'''    <Label resid="{line['id']}.label"/>''')       
@@ -44,14 +40,22 @@ with open("manifest_bs4.xml", "w") as f:
                         group_xml.append(f'''      <Description resid="{line['id']}.Desc"/>''')       
                         group_xml.append( '''    </Supertip>''')
                         group_xml.append( '''    <Icon>''')
-                        group_xml.append(f'''      <bt:Image size="16" resid="IconSelectArea.16x16"/>''')
-                        group_xml.append(f'''      <bt:Image size="32" resid="IconSelectArea.32x32"/>''')
-                        group_xml.append(f'''      <bt:Image size="80" resid="IconSelectArea.80x80"/>''')
+                        group_xml.append(f'''      <bt:Image size="16" resid="{line['imageMso']}.16x16"/>''')
+                        group_xml.append(f'''      <bt:Image size="32" resid="{line['imageMso']}.32x32"/>''')
+                        group_xml.append(f'''      <bt:Image size="80" resid="{line['imageMso']}.80x80"/>''')
                         group_xml.append( '''    </Icon>''')
                         group_xml.append( '''    <Action>''')
                         group_xml.append( '''      <FunctionName>OnAction_ECAM</FunctionName>''')
                         group_xml.append( '''    </Action>''')
                         group_xml.append( '''  </Control>''')
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/IconSelectArea_16.png"/>''')
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/IconSelectArea_32.png"/>''')
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/IconSelectArea_80.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_16.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_32.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_80.png"/>''')
+
+                        res_xml['ShortStrings'].append(f'''<bt:String id="{line['id']}.label" DefaultValue="{line['label']}"/>''')
                     elif line.name == 'menu':
                         group_xml.append(f'''  <Control xsi:type="Menu" id="{line['id']}">''')
                         group_xml.append(f'''    <Label resid="{line['id']}.label"/>''')       
@@ -60,11 +64,20 @@ with open("manifest_bs4.xml", "w") as f:
                         group_xml.append(f'''      <Description resid="{line['id']}.Desc"/>''')       
                         group_xml.append( '''    </Supertip>''')
                         group_xml.append( '''    <Icon>''')
-                        group_xml.append(f'''      <bt:Image size="16" resid="IconSelectArea.16x16"/>''')
-                        group_xml.append(f'''      <bt:Image size="32" resid="IconSelectArea.32x32"/>''')
-                        group_xml.append(f'''      <bt:Image size="80" resid="IconSelectArea.80x80"/>''')
+                        group_xml.append(f'''      <bt:Image size="16" resid="{line['imageMso']}.16x16"/>''')
+                        group_xml.append(f'''      <bt:Image size="32" resid="{line['imageMso']}.32x32"/>''')
+                        group_xml.append(f'''      <bt:Image size="80" resid="{line['imageMso']}.80x80"/>''')
                         group_xml.append( '''    </Icon>''')
                         group_xml.append( '''    </Items>''')
+
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/IconSelectArea_16.png"/>''')
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/IconSelectArea_32.png"/>''')
+                        res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/IconSelectArea_80.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_16.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_32.png"/>''')
+                        # res_xml['Images'].append(f'''<bt:Image id="{line['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/{line['imageMso']}_80.png"/>''')
+
+                        res_xml['ShortStrings'].append(f'''<bt:String id="{line['id']}.label" DefaultValue="{line['label']}"/>''')
                         for item in line.contents:
                             if type(item) == Tag:
                                 group_xml.append(f'''      <Item id="{item['id']}">''')
@@ -73,15 +86,28 @@ with open("manifest_bs4.xml", "w") as f:
                                 group_xml.append(f'''          <Title resid="{item['id']}.Title"/>''')       
                                 group_xml.append(f'''          <Description resid="{item['id']}.Desc"/>''')       
                                 group_xml.append( '''        </Supertip>''')
-                                group_xml.append( '''        <Icon>''')
-                                group_xml.append(f'''          <bt:Image size="16" resid="IconSelectArea.16x16"/>''')
-                                group_xml.append(f'''          <bt:Image size="32" resid="IconSelectArea.32x32"/>''')
-                                group_xml.append(f'''          <bt:Image size="80" resid="IconSelectArea.80x80"/>''')
-                                group_xml.append( '''        </Icon>''')
+                                if 'imageMso' in item:
+                                    group_xml.append( '''        <Icon>''')
+                                    group_xml.append(f'''          <bt:Image size="16" resid="{item['imageMso']}.16x16"/>''')
+                                    group_xml.append(f'''          <bt:Image size="32" resid="{item['imageMso']}.32x32"/>''')
+                                    group_xml.append(f'''          <bt:Image size="80" resid="{item['imageMso']}.80x80"/>''')
+                                    group_xml.append( '''        </Icon>''')
                                 group_xml.append( '''        <Action>''')
                                 group_xml.append( '''          <FunctionName>OnAction_ECAM</FunctionName>''')
                                 group_xml.append( '''        </Action>''')
                                 group_xml.append( '''      </Item>''')
+
+                                if 'imageMso' in item:
+                                    res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/IconSelectArea_16.png"/>''')
+                                    res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/IconSelectArea_32.png"/>''')
+                                    res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/IconSelectArea_80.png"/>''')
+                                    # res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.16x16" DefaultValue="https://localhost:3000/assets/{item['imageMso']}_16.png"/>''')
+                                    # res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.32x32" DefaultValue="https://localhost:3000/assets/{item['imageMso']}_32.png"/>''')
+                                    # res_xml['Images'].append(f'''<bt:Image id="{item['imageMso']}.80x80" DefaultValue="https://localhost:3000/assets/{item['imageMso']}_80.png"/>''')
+                                # else  an icon is required point each of these to the icon used for group that displays blank
+
+                                res_xml['ShortStrings'].append(f'''<bt:String id="{item['id']}.label" DefaultValue="{item['label']}"/>''')
+
                         group_xml.append( '''        </Items>''')
                         group_xml.append( '''  </Control>''')
                         
