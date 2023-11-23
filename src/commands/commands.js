ad5564e386dafadfa7704b621114e96b77000537
MichaelHBaker <mbaker@sbwconsulting.com>
2023-11-23 10:33:49 -0800

    event id written to range

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -19,7 +19,7 @@ async function highlightSelection(event) {
       await Excel.run(async (context) => {
           const range = context.workbook.getSelectedRange();
           range.format.fill.color = "yellow";
-          range.values = "ok"
+          range.values = "ok";
           await context.sync();
       });
   } catch (error) {
@@ -34,5 +34,29 @@ async function highlightSelection(event) {
 // You must register the function with the following line.
 Office.actions.associate("highlightSelection", highlightSelection);
 
+// The command function.
+async function OnAction_ECAM(event) {
+  // Implement your custom code here. The following code is a simple Excel example.  
+  try {
+    await Excel.run(async (context) => {
+          const range = context.workbook.getSelectedRange();
+          range.format.fill.color = "yellow";
+          // range.values = "OnAction_ECAM";
+          range.values = event.source['id'];
+          await context.sync();
+          console.log("hello world");
+          console.log(event);
+      });
+  } catch (error) {
+      // Note: In a production add-in, notify the user through your add-in's UI.
+      console.error(error);
+  }
+
+  // Calling event.completed is required. event.completed lets the platform know that processing has completed.
+  event.completed();
+}
+
+// You must register the function with the following line.
+Office.actions.associate("OnAction_ECAM", OnAction_ECAM);
 
 
