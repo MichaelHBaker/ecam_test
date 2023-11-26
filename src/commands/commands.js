c2abfda63c6e1bbd42d732d6470c62983cd918a2
MichaelHBaker <mbaker@sbwconsulting.com>
2023-11-26 12:33:14 -0800

    start dialog coding

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,51 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global global, Office, self, window */
+/* global global, Excel, console, Office, self, window */
 
 Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
 
-
-// The command function.
-async function highlightSelection(event) {
-  // Implement your custom code here. The following code is a simple Excel example.  
-  try {
-      await Excel.run(async (context) => {
-          const range = context.workbook.getSelectedRange();
-          range.format.fill.color = "yellow";
-          range.values = "ok";
-          await context.sync();
-      });
-  } catch (error) {
-      // Note: In a production add-in, notify the user through your add-in's UI.
-      console.error(error);
-  }
-
-  // Calling event.completed is required. event.completed lets the platform know that processing has completed.
-  event.completed();
-}
-
-// You must register the function with the following line.
-Office.actions.associate("highlightSelection", highlightSelection);
-
 // The command function.
 async function OnAction_ECAM(event) {
-  // Implement your custom code here. The following code is a simple Excel example.  
+
   try {
     await Excel.run(async (context) => {
           const range = context.workbook.getSelectedRange();
-          range.format.fill.color = "yellow";
-          // range.values = "OnAction_ECAM";
           range.values = event.source['id'];
-          await context.sync();
-          console.log("hello world");
-          console.log(event);
+          openDialog(event.source['id']);
       });
   } catch (error) {
       // Note: In a production add-in, notify the user through your add-in's UI.
@@ -59,4 +27,22 @@ async function OnAction_ECAM(event) {
 // You must register the function with the following line.
 Office.actions.associate("OnAction_ECAM", OnAction_ECAM);
 
+function openDialog(message) {
+  // URL of your dialog HTML page
+  const dialogUrl = 'https://localhost:3000/messageDialog.html'; 
+  Office.context.ui.displayDialogAsync(dialogUrl, { width: 20, height: 40 }, function (asyncResult) {
+      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+          var dialog = asyncResult.value;
+          dialog.addEventHandler(Office.EventType.DialogMessageReceived, processMessageFromDialog);
 
+          // You can also send an initial message to your dialog here, if needed
+          dialog.messageChild({ type: "initialMessage", value: "Hello Dialog!" });
+      } else {
+          console.error("Failed to open dialog: " + asyncResult.error.message);
+      }
+  });
+}
+
+function processMessageFromDialog(arg) {
+  console.log("Message from dialog: " + arg.message); // Handle messages sent from the dialog
+}
