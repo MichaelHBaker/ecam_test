5f7f26164f16ae25a55955ee1cb8f331f70ba2e9
MichaelHBaker <mbaker@sbwconsulting.com>
2023-12-25 12:45:31 -0800

    dialog hello

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -11,79 +11,22 @@ async function OnAction_ECAM(event) {
 
   try {
     await Excel.run(async (context) => {
-          const range = context.workbook.getSelectedRange();
-          range.values = event.source['id'];
-
-          // await context.sync();
-          console.log(event.source['id']);
-          console.log("hello world");
-
-          // openDialog(event.source['id']);
           OpenDialog("This is the message to display in the dialog box.");
       });
   } catch (error) {
-      // Note: In a production add-in, notify the user through your add-in's UI.
       console.error(error);
   }
 
-  // Calling event.completed is required. event.completed lets the platform know that processing has completed.
   event.completed();
 }
 
-// You must register the function with the following line.
-Office.actions.associate("OnAction_ECAM", OnAction_ECAM);
-
-// function OpenDialog(message) {
-//   Office.context.ui.displayDialogAsync(
-//     'https://your-add-in-domain/dialog.html', // Replace with the URL of your dialog page
-//     { height: 400, width: 500 },
-//     asyncResult => {
-//       if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-//         const dialog = asyncResult.value;
-//         dialog.addEventHandler(Office.EventType.DialogMessageReceived, event => {
-//           // Handle messages sent back from the dialog, if needed
-//           console.log(event.message);
-//         });
-//         // Pass the message to the dialog page
-//         dialog.messageParent(message);
-//       } else {
-//         console.error('Error opening dialog:', asyncResult.error.message);
-//       }
-//     }
-//   );
-// }
 
 function OpenDialog(message) {
-  // URL of your dialog HTML page
-  // const dialogUrl = 'https://localhost:3000/messageDialog.html'; 
   const dialogUrl = 'https://localhost:3000/popup.html'; 
-  Office.context.ui.displayDialogAsync(dialogUrl, { width: 20, height: 40 }, async function (asyncResult) {
-      if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-          var dialog = asyncResult.value;
-          dialog.addEventHandler(Office.EventType.DialogMessageReceived, function (arg) {
-            console.log("Message from dialog: " + arg.message); // Handle messages sent from the dialog
-            dialog.messageChild("Hello Dialog!");
-            console.log("message sent ");
-          
-          });
 
-          // You can also send an initial message to your dialog here, if needed
-          // dialog.messageChild({ type: "initialMessage", value: "Hello Dialog!" });
-          // console.log("dialog before sleep: " + Object.getOwnPropertyNames(dialog));
-          // await new Promise(r => setTimeout(r, 1000));
-          // console.log("dialog after sleep: " + Object.getOwnPropertyNames(dialog));
-          // dialog.messageChild("Hello Dialog!");
-          // console.log("message sent ");
+  Office.context.ui.displayDialogAsync(dialogUrl, { height: 30, width: 20 },
+  (asyncResult) => {
+      const dialog = asyncResult.value;
+      });
+  }
 
-        } else {
-          console.error("Failed to open dialog: " + asyncResult.error.message);
-      }
-  });
-}
-
-function processMessageFromDialog(arg) {
-  console.log("Message from dialog: " + arg.message); // Handle messages sent from the dialog
-  dialog.messageChild("Hello Dialog!");
-  console.log("message sent ");
-
-}
