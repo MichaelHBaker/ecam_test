2bf9b33be0131a7309a72fa3e45df8ed980c9602
MichaelHBaker <mbaker@sbwconsulting.com>
2024-01-17 11:36:54 -0800

    address not written to input element

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -4,7 +4,41 @@
 
 
 Office.onReady((info) => {
-    
+  if (info.host === Office.HostType.Excel) {
+
+    // Assign event handlers and other initialization logic.
+    document.getElementById("range_add_id").onclick = getAddress;
+  }
 
   });
 
+async function getAddress(event){
+  // Additional Excel.run can be placed here if needed
+  try {
+    await Excel.run(async (context) => {
+      // Asynchronous Excel operations here
+      const worksheet = context.workbook.worksheets.getActiveWorksheet();
+  
+      worksheet.onSelectionChanged.add(changeHandler);
+      await context.sync();
+      console.log("Event handler added");
+    }); 
+  }
+  catch(error){
+    // need to figure out how to display dialogs on all catches
+    console.error(error);
+  }
+
+}
+
+async function changeHandler(event){
+  await Excel.run(async (context) => {
+    const worksheet = context.workbook.worksheets.getActiveWorksheet();
+    await context.sync();
+    
+    document.getElementById("range_add_id").value = worksheet.name + "!" + event.address;
+
+    console.log("event happended - address" + event.address);
+    console.log("event happended - source" + event.source);
+  });
+}
